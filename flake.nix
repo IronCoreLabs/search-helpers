28ab00975b6a51cd9d6577b89a108e73cca155d1
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2025-05-15 10:29:28 -0700

    Add flake, linting (#43)

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A build for search-helpers";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rusttoolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [ rusttoolchain pkg-config ];
+        };
+      });
+}
